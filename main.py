6849f597498df4556ba5bed91765bad55f817a9c
Ilia Perfilov <ind.climb@gmail.com>
2023-02-27 20:40:24 +0300

    modified - added new functions

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import utils as ut
 
-OPERATION = ut.load_all_operation()
 
+operations = ut.get_n_operation()
 
+for operation in operations:
+    date_operation = ut.get_date(operation['date'])
+    descr_operation = operation.get('description')
+    from_operation = operation.get('from')
+    to_operation = operation.get('to')
+    amount = operation['operationAmount']['amount']
+    currency = operation['operationAmount']['currency']['name']
+    print(date_operation, descr_operation)
