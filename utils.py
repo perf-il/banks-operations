ff6b5e91e2351b394edd02bf23a48b326155d95b
Ilia Perfilov <ind.climb@gmail.com>
2023-02-27 20:19:45 +0300

    modified - added new functions

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+from _datetime import datetime
 
 
 def load_all_operation():
@@ -22,7 +23,27 @@ def get_n_operation(n=5):
     return n_operation[:n]
 
 
-#for i in get_n_operation():
-#    print(i['date'], i['description'])
-#    print(i.get('from'), i.get('to'))
-#   print(i['operationAmount']['amount'], i['operationAmount']['currency']['name'], '\n')
+def get_date(date):
+    new_date = datetime.strptime(date[:10], '%Y-%m-%d')
+    return new_date.strftime('%d.%m.%Y')
+
+
+def get_secret_card(name_card):
+    name = name_card[:-16]
+    number = name_card[-16:]
+    return f'{name}{number[:6]}******{number[-4:]}'
+
+
+def get_secret_bill(name_bill):
+    return f'{name_bill[:4]} **{name_bill[-4:]}'
+
+
+'''
+
+for i in get_n_operation():
+    print(get_date(i['date']), i['description'])
+    print(get_secret_card(i.get('from')), get_secret_bill(i.get('to'))) if i.get('from') is not None else print(get_secret_bill(i.get('to')))
+    print(i['operationAmount']['amount'], i['operationAmount']['currency']['name'], '\n')
+
+    
+'''
