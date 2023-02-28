6e32e70b64234fd094105f069ba87e4da1fe047d
Ilia Perfilov <ind.climb@gmail.com>
2023-02-28 13:58:44 +0300

    modified main.py - created stability version

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import utils as ut
 
 
-operations = ut.get_n_operation()
-
-for operation in operations:
+for operation in ut.get_n_operation():
     date_operation = ut.get_date(operation['date'])
     descr_operation = operation.get('description')
     from_operation = operation.get('from')
@@ -11,3 +9,12 @@ for operation in operations:
     amount = operation['operationAmount']['amount']
     currency = operation['operationAmount']['currency']['name']
     print(date_operation, descr_operation)
+    if from_operation is not None:
+        if ut.is_bill(from_operation):
+            print(f'{ut.get_secret_bill(from_operation)} -> {ut.get_secret_bill(to_operation)}')
+        else:
+            print(f'{ut.get_secret_card(from_operation)} -> {ut.get_secret_bill(to_operation)}')
+    else:
+        print(f'{ut.get_secret_bill(to_operation)}')
+    print(amount, currency, '\n')
+
